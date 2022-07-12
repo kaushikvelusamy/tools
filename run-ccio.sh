297559109dc54d59b31cdc033fd17c4f98142d4d
kaushikvelusamy <kaushikvelusamy@gmail.com>
2022-07-12 11:25:55 -0500

    replaced mac to local and fixed exerciser compile.

--- run-ccio.sh
+++ run-ccio.sh
@@ -1,8 +1,8 @@
 # Bash script to clone,compile and run ccio feature of HDF5
 # Author: Kaushik 
 # Date: Apr 27, 2022
-# Example : sh ./run-ccio.sh -s "1 2" -b ccio-v2 -m mac -d debug -p 0
-# Example : sh ./run-ccio.sh -s "2" -m mac -d debug -p 0 
+# Example : sh ./run-ccio.sh -s "1 2" -b ccio-v2 -m local -d debug -p 0
+# Example : sh ./run-ccio.sh -s "2" -m local -d debug -p 0 
 # Example : sh ./run-ccio.sh -s "2" -m theta -d debug -p 1 
 # Example : sh ./run-ccio.sh -s 4   //To run quick recompiling
 # Example : sh ./run-ccio.sh -c 1   //To make clean
@@ -19,7 +19,7 @@ export HDF5_ROOT=$(pwd)"/.."
 echo $HDF5_ROOT
 export CRAYPE_LINK_TYPE=dynamic
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDF5_ROOT/library/install/ccio/lib
-#use mpicc(nompirun-debug/mac) or cc (prod-pmi.h) or h5pcc 
+#use mpicc(nompirun-debug/local) or cc (prod-pmi.h) or h5pcc 
 mycompiler="mpicc"
 #extra arguments for hdf5 compile " --enable-shared --enable-threadsafe --enable-unsupported --enable-map-api"
 
@@ -62,7 +62,7 @@ stage2()
           echo "debug incorrect"
           exit 0
       fi
-  elif [ "$machine" = "mac" ]; then
+  elif [ "$machine" = "local" ]; then
       cd $HDF5_ROOT/gitrepos/hdf5
       ./autogen.sh
       cd $HDF5_ROOT/library/build/ccio
@@ -94,33 +94,38 @@ stage2()
 
 stage3()
 {
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDF5_ROOT/library/install/ccio/lib
+
   echo "Starting scripts for Exerciser test code setup"
   HDF5_INSTALL_DIR=${HDF5_ROOT}/library/install/ccio
   cd $HDF5_ROOT/gitrepos
-  git clone https://xgitlab.cels.anl.gov/kvelusamy/BuildAndTest.git 
-
-  if [[ ! -e $HDF5_ROOT/exerciser/ccio ]]; then
+  if [ ! -e $HDF5_ROOT/gitrepos/BuildAndTest ]; then
+    git clone https://xgitlab.cels.anl.gov/kvelusamy/BuildAndTest.git 
+  fi
+  
+  if [ ! -e $HDF5_ROOT/exerciser/ccio ]; then
     mkdir -p $HDF5_ROOT/exerciser/ccio
-  elif [[ ! -d $HDF5_ROOT/exerciser/ccio ]]; then
+  elif [ ! -d $HDF5_ROOT/exerciser/ccio ]; then
     echo "$HDF5_ROOT/exerciser/ccio already exists" 1>&2
   fi
 
   cd $HDF5_ROOT/exerciser/ccio
   cp $HDF5_ROOT/gitrepos/BuildAndTest/Exerciser/exerciser.c .
 
-  if [ "$machine" == "theta" ]; then
+  if [ "$machine" = "theta" ]; then
     cp $HDF5_ROOT/gitrepos/BuildAndTest/Exerciser/Theta/Makefile.theta .
     module swap craype-haswell craype-mic-knl
     export CRAYPE_LINK_TYPE=static
     make -f Makefile.theta
-  elif [ $machine == "mac" -o $machine == "theta" ]; then
+  elif [ "$machine" = "local" ]; then
     cp $HDF5_ROOT/gitrepos/BuildAndTest/Exerciser/Theta/Makefile.mac .
+
     make -f Makefile.mac 
   fi
 
-  if [[ ! -e $HDF5_ROOT/exerciser/run ]]; then
+  if [ ! -e $HDF5_ROOT/exerciser/run ]; then
     mkdir -p $HDF5_ROOT/exerciser/run
-  elif [[ ! -d $HDF5_ROOT/exerciser/run ]]; then
+  elif [ ! -d $HDF5_ROOT/exerciser/run ]; then
     echo "$HDF5_ROOT/exerciser/run already exists" 1>&2
   fi
 
@@ -143,7 +148,7 @@ usage()
 { 
 echo "Usage: $0 [-s stages <1|2|3|2 3|..>] 
     \t\t  [-b <develop|ccio|ccio-v2>] 
-    \t\t  [-m <mac|theta>]
+    \t\t  [-m <local|theta>]
     \t\t  [-d <prod|debug>]
     \t\t  [-p <0-serialmake|1-parallelmake]
     \t\t  [-c 1- cleaneverything]
@@ -175,7 +180,7 @@ do
         m)
           machine=${OPTARG} 
           echo $machine
-          if [ $machine = "mac" -o $machine = "theta" ]
+          if [ $machine = "local" -o $machine = "theta" ]
           then
             echo "machine: $machine";
           else
